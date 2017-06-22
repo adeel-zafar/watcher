bc2c8160dcc82699a4324ca3399b68376a793d23
Adeel Zafar <adeelzafar2008@gmail.com>
2017-06-22 12:38:03 +0530

    Added JS assets

--- assets/static/dev/javascript/index.js
+++ assets/static/dev/javascript/index.js
@@ -1,6 +1,34 @@
-var data = {a : 1};
+const tabular = {
+  template: '<div>Tabular</div>'
+}
 
-var vm = new Vue({
-  el: "#watcher",
-  data : data
+const visual = {
+  template: '<div>Visual</div>'
+}
+
+const root = {
+  template: '<div>Input</div>'
+}
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: root,
+      children: [
+        {
+          path: 'tabular',
+          component: tabular
+        },
+        {
+          path: 'visual',
+          component: visual
+        }
+      ]
+    }
+  ]
 });
+
+const app = new Vue({
+  router
+}).$mount('#app')
--- watcher.go
+++ watcher.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"log"
-	"io"
 	"bytes"
-	"net/http"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
 	h := http.NewServeMux()
-	h.Handle("/watcher", http.StripPrefix("/watcher", http.HandlerFunc(watcher_handler)))
-	h.Handle("/", http.StripPrefix("/", http.HandlerFunc(assets_handler)))
+	h.Handle("/watcher", http.StripPrefix("/watcher", http.HandlerFunc(watcherHandler)))
+	h.Handle("/", http.StripPrefix("/", http.HandlerFunc(assetsHandler)))
 
 	err := http.ListenAndServe(":8080", h)
 	log.Fatal(err)
 
 }
 
-func watcher_handler(w http.ResponseWriter, r *http.Request) {
+func watcherHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "200")
 }
 
-func assets_handler(w http.ResponseWriter, r *http.Request) {
-	var path string = r.URL.Path
-  if path == "" {
-    path = "html/index.html"
-  }
-  if bs, err := Asset(path); err != nil {
-    w.WriteHeader(http.StatusNotFound)
-  } else {
-    var reader = bytes.NewBuffer(bs)
-    io.Copy(w, reader)
-  }
+func assetsHandler(w http.ResponseWriter, r *http.Request) {
+	var path = r.URL.Path
+	if path == "" {
+		path = "html/index.html"
+	}
+	if bs, err := Asset(path); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		var reader = bytes.NewBuffer(bs)
+		io.Copy(w, reader)
+	}
 }
